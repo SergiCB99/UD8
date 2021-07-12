689610ba09a8e2a1443f5ab051b548054cd54797
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-07-12 18:18:14 +0200

    Create Ex2App.java

---
+++ Ex2/Ex2App.java
@@ -0,0 +1,14 @@
+
+public class Ex2App {
+
+	public static void main(String[] args) {
+		
+		Password password1 = new Password ();
+		Password password2 = new Password (5);
+		
+		System.out.println(password1.toString());
+		System.out.println(password2.toString());
+		
+	}
+	
+}
